369b6f096614a5549dbaf61bf89c126888e82486
Philip Kiely <philip.kiely@baseten.co>
2023-12-14 13:13:52 -0600

    lint this too

--- libs/community/tests/integration_tests/llms/test_baseten.py
+++ libs/community/tests/integration_tests/llms/test_baseten.py
@@ -5,6 +5,7 @@ from langchain_community.llms.baseten import Baseten
 
 # This test requires valid BASETEN_MODEL_ID and BASETEN_API_KEY environment variables
 
+
 def test_baseten_call() -> None:
     """Test valid call to Baseten."""
     llm = Baseten(model=os.environ["BASETEN_MODEL_ID"])
